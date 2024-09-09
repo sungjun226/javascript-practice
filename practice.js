7dffc1901e6a5f760bb051bd6b3cf8f5c41696e6
sungjun <joondol600@gmail.com>
2024-09-09 11:23:31 +0900

    javascript practice

---
+++ practice.js
@@ -0,0 +1,35 @@
+let name = "sungjun"
+let age = 22
+let residence = "busan"
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof residence)
+
+let a = 30
+let b = 10
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
+let firstName = "lim"
+let lastName = "sungjun"
+
+console.log(firstName , lastName)
+
+let inputAge = 17
+
+if (inputAge >= 18) {
+    console.log("성인입니다")
+}else {
+    console.log("미성년자 입니다.")
+}
+
+let adult = "성인"
+let child = "미성년자"
+
+console.log(inputAge >= 18 ? adult : child)
